6966d52dc61b62f7fc5adffe813ff086e69e5be4
Amogh Nayak <amoghnk03@gmail.com>
2016-12-21 15:47:44 -0500

    final heroku launch

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn myproject.wsgi --log-file -
+web: gunicorn currencyExchange.wsgi
--- src/currencyExchange/wsgi.py
+++ src/currencyExchange/wsgi.py
@@ -4,7 +4,7 @@ WSGI config for currencyExchange project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
@@ -14,5 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "currencyExchange.settings")
 
 application = get_wsgi_application()
-
-
