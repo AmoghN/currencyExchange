00f39c340267f75ac6dc4b83a77a8ffe536afefe
Amogh Nayak <amoghnk03@gmail.com>
2017-06-11 12:46:47 -0400

    complete file restructure

--- currencyExchange/currencyExApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^result$', views.result, name='result'),
-    url(r'^hresult$', views.hresult, name='hresult')    
-]
--- currencyExchange/settings.py
+++ currencyExchange/settings.py
@@ -119,9 +119,6 @@ DATABASES['default'].update(db_from_env)
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
